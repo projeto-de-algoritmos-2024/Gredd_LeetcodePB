1ea29f247d1f0bd93f1807033c46a69bb2fc2644
Flávio Melo <flavioovatsug@gmail.com>
2025-01-19 17:02:29 -0300

    Adiciona questão dificil 2, versão inicial

---
+++ dificeis/Question2_dificil.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def candy(self, ratings):
+        # array para armazenar os doces, com 1 doce para cada criança inicialmente.
+        arr = [1] * len(ratings)
+
+
+        return sum(arr)
