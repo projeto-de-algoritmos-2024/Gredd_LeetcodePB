4668236add2d98e0a87e76c7eac50aa80ab5f177
Flávio Melo <flavioovatsug@gmail.com>
2025-01-19 16:55:21 -0300

    Final questão média

--- medias/Question2_media.py
+++ medias/Question2_media.py
@@ -4,3 +4,13 @@ class Solution(object):
         if total_gas < total_cost:
             return -1 
 
+        current_gas, start = 0, 0
+
+        for i in range(len(gas)):
+            current_gas += gas[i] - cost[i]
+
+            if current_gas < 0:
+                start = i + 1
+                current_gas = 0 
+
+        return start
