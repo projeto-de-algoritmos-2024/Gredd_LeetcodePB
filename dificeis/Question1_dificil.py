1c4859492c9d36fe2a7472fb3193456e6fb95059
Harry_cmartin <harrysoncampos@gmail.com>
2025-01-19 14:00:41 -0300

    Question1_hard

--- dificeis/Question1_dificil.py
+++ dificeis/Question1_dificil.py
@@ -3,5 +3,19 @@ class Solution(object):
         n = len(ratings)
         if n == 0:
             return 0
+        # Array para armazenar a quantidade de doces que cada criança receberá
+        candies = [1] * n
 
+        # Passo 1: Varredura da esquerda para a direita
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+
+        # Passo 2: Varredura da direita para a esquerda
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+
+        # Soma total dos doces
+        return sum(candies)
         
